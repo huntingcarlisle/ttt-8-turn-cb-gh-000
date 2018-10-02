cabc74bbbc0ddcf3433e3ccaeecb08a859896a12
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-10-02 20:09:50 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -18,3 +18,12 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+def input_to_index(input)
+  return input.to_i - 1
+end  
+
+def move(board, index, character = "X")
+  board[index] = character
+  return board
+end
